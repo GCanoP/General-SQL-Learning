a5d7c0c4e27ac8478345c8b7f72fe596859592a2
Gerardo Cano <gerardocano18@gmail.com>
2022-07-26 20:33:01 -0500

    folders-renamed

---
+++ SQL Notes for Professionals Code/Example Select Commands.sql
@@ -0,0 +1,13 @@
+use database_example; 
+
+-- SELECT COMMANDS. 
+
+-- Select Individual columns. 
+SELECT
+	PhoneNumber,
+    Email,
+    PreferredContact
+FROM Customers;
+
+-- Select specific number of records. 
+
